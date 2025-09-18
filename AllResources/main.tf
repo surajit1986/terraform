94e233928d0953a8dfc4226c5915c1eae4d6902e
Surajit Ghosh <surajit.1986@gmail.com>
2025-09-18 15:47:19 +0000

    network interce with subnet using tf

--- AllResources/main.tf
+++ AllResources/main.tf
@@ -11,40 +11,51 @@ provider "azurerm" {
     features {
       
     }  
+ 
 
+}
 
+locals {
+  rg_name = "app-grp"
+  location ="West Europe"
+  sg_name = "app-network-sg"
+  network_name = "app-network"
+  subnet_1_name = "websubnet01"
+  subnet_2_name = "appsubnet02"
 }
 
 resource "azurerm_resource_group" "appgrp" {
-  name     = "app-grp"
-  location = "West Europe"
+  name     = local.rg_name
+  location = local.location
 }
 
-resource "azurerm_storage_account" "appstore" {
-  name                     = "terraformappstore"
-  resource_group_name      = azurerm_resource_group.appgrp.name
-  location                 = azurerm_resource_group.appgrp.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+resource "azurerm_network_security_group" "appnetworksg" {
+  name                = local.sg_name
+  location            = azurerm_resource_group.appgrp.location
+  resource_group_name = azurerm_resource_group.appgrp.name
+  depends_on = [ azurerm_resource_group.appgrp ]
+}
+
+resource "azurerm_virtual_network" "appnetwork" {
+  name                = local.network_name
+  location            = azurerm_resource_group.appgrp.location
+  resource_group_name = azurerm_resource_group.appgrp.name
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["10.0.0.4", "10.0.0.5"]
   depends_on = [ azurerm_resource_group.appgrp ]
 
-  tags = {
-    environment = "terraform-dev"
+  subnet {
+    name             = local.subnet_1_name
+    address_prefixes = ["10.0.1.0/24"]
   }
-}
 
-resource "azurerm_storage_container" "tfblobcontainer" {
-  name                  = "tfblobcontainer"
-  storage_account_id    = azurerm_storage_account.appstore.id
-  container_access_type = "blob"
-  depends_on = [ azurerm_storage_account.appstore ]
-}
+  subnet {
+    name             = local.subnet_2_name
+    address_prefixes = ["10.0.2.0/24"]
+    security_group   = azurerm_network_security_group.appnetworksg.id
+  }
 
-resource "azurerm_storage_blob" "tfstorageblob" {
-  name                   = "myPic.jpg"
-  storage_account_name   = azurerm_storage_account.appstore.name
-  storage_container_name = azurerm_storage_container.tfblobcontainer.name
-  type                   = "Block"
-  source                 = "myPic.jpg"
-  depends_on = [ azurerm_storage_container.tfblobcontainer, azurerm_storage_account.appstore ]
+  tags = {
+    environment = "dev"
+  }
 }
