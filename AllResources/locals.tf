de97a9ae977f80b1db19c09820dd2f3fa40d4c51
Surajit Ghosh <surajit.1986@gmail.com>
2025-09-20 16:53:55 +0000

    added subnet work

--- AllResources/locals.tf
+++ AllResources/locals.tf
@@ -3,6 +3,18 @@ locals {
   location ="West Europe"
   sg_name = "app-network-sg"
   network_name = "app-network"
-  subnet_1_name = "websubnet01"
-  subnet_2_name = "appsubnet02"
+  address_spaces = ["10.0.0.0/16", "10.0.0.4", "10.0.0.5"]
+
+  subnets=[
+    {
+      name = "websubnet01"
+      address_prefixes = ["10.0.1.0/24"]
+    },
+     {
+      name = "appsubnet02"
+      address_prefixes = ["10.0.2.0/24"]
+    }
+  ]
+
+  network_interface_name = "appnetworkinterface"
 }
--- AllResources/main.tf
+++ AllResources/main.tf
@@ -15,22 +15,37 @@ resource "azurerm_virtual_network" "appnetwork" {
   name                = local.network_name
   location            = azurerm_resource_group.appgrp.location
   resource_group_name = azurerm_resource_group.appgrp.name
-  address_space       = ["10.0.0.0/16"]
-  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+  address_space       = [local.address_spaces[0]]
+  dns_servers         = [local.address_spaces[1], local.address_spaces[2]]
   depends_on = [ azurerm_resource_group.appgrp ]
-
-  subnet {
-    name             = local.subnet_1_name
-    address_prefixes = ["10.0.1.0/24"]
-  }
-
-  subnet {
-    name             = local.subnet_2_name
-    address_prefixes = ["10.0.2.0/24"]
-    security_group   = azurerm_network_security_group.appnetworksg.id
-  }
 
   tags = {
     environment = "dev"
+  }
+}
+
+resource "azurerm_subnet" "websubet01" {
+  name                 = local.subnets[0].name
+  resource_group_name  = azurerm_resource_group.appgrp.name
+  virtual_network_name = azurerm_virtual_network.appnetwork.name
+  address_prefixes     = local.subnets[0].address_prefixes
+}
+
+resource "azurerm_subnet" "appsubnet01" {
+  name                 = local.subnets[1].name
+  resource_group_name  = azurerm_resource_group.appgrp.name
+  virtual_network_name = azurerm_virtual_network.appnetwork.name
+  address_prefixes     = local.subnets[1].address_prefixes
+}
+
+resource "azurerm_network_interface" "appnetworkinterface" {
+  name                = local.network_interface_name
+  location            = azurerm_resource_group.appgrp.location
+  resource_group_name = azurerm_resource_group.appgrp.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.websubet01.id
+    private_ip_address_allocation = "Dynamic"
   }
 }
